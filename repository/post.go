b20fd456005ec955fe51e6f55f99694904b1862c
yhmain <yhmain@gmail.com>
2022-05-09 19:07:46 +0800

    Dao insert new post

--- repository/post.go
+++ repository/post.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"encoding/json"
+	"os"
 	"sync"
 )
 
@@ -15,8 +17,9 @@ type PostDao struct {
 }
 
 var (
-	postDao  *PostDao  //指向Dao结构体的指针变量
-	postOnce sync.Once //单例模式，可以在代码的任意位置初始化和调用，因此可以延迟到使用时再执行，并发场景下是线程安全的
+	postDao  *PostDao     //指向Dao结构体的指针变量
+	postOnce sync.Once    //单例模式，可以在代码的任意位置初始化和调用，因此可以延迟到使用时再执行，并发场景下是线程安全的
+	rwMutex  sync.RWMutex //支持单写多读，而sync.Mutex：互斥锁
 )
 
 //初始化单例
@@ -32,4 +35,30 @@ func NewPostDaoInstance() *PostDao {
 //func (*PostDao)表示为 *PostDao的成员方法
 func (*PostDao) QueryPostsByParentId(parentId int64) []*Post {
 	return postIndexMap[parentId]
+}
+
+//向文件/数据库中插入新发布的帖子
+func (*PostDao) InsertNewPost(post *Post) error {
+	open, err := os.Open("./data/post")
+	if err != nil {
+		return err
+	}
+	defer open.Close()
+	postStr, _ := json.Marshal(post) //序列号为json格式
+	//写入文件时需要string
+	if _, err = open.WriteString(string(postStr) + "\n"); err != nil {
+		return err
+	}
+	//更新Map时考虑并发安全性问题
+	rwMutex.Lock()
+	postList, ok := postIndexMap[post.ParentId]
+	//若字典中不存在该主题ID，则新建
+	if !ok {
+		postIndexMap[post.ParentId] = []*Post{post}
+	} else {
+		postList = append(postList, post)
+		postIndexMap[post.ParentId] = postList
+	}
+	rwMutex.Unlock()
+	return nil
 }
--- service/submit_post.go
+++ service/submit_post.go
@@ -1,1 +1,7 @@
 package service
+
+import (
+	idworker "github.com/gitstliu/go-id-worker"
+)
+
+var idGen *idworker.IdWorker
